02a5514b6d32ae16142f6be90e1d2e67baac9060
HP <HP@FENGYANGLUO>
2025-12-06 17:09:25 +0800

    1. 修改一个注释

--- SpeechCompetition/Speaker.cpp
+++ SpeechCompetition/Speaker.cpp
@@ -5,7 +5,9 @@ Speaker::Speaker(string name)
 {
 	this->m_Name = "";
 	//数组不需要传参啊，直接元素=0就行了
-	this->m_Score[] = { 0.0,0.0 };???
+	//this->m_Score[] = { 0.0,0.0 }; 这种初始化列表的形式这里不可以用。但是还是要注意数组要 []，且 double 类型的 0 也要写成 0.0
+	this->m_Score[0] = 0;
+	this->m_Score[1] = 0;
 }
 
 //析构函数
